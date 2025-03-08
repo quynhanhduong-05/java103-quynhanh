583ada79c31437041cfbce6bc246b06c6babeda9
chouchou <duongquynhanh1807@gmail.com>
2025-03-08 21:49:06 +0900

    Add folder ss4

---
+++ ss4_oop/stop-watch/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[10000];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 10000);
+        }
+
+        StopWatch stopWatch = new StopWatch();
+
+        stopWatch.start();
+        selectionSort(arr);
+        stopWatch.stop();
+        System.out.println(stopWatch.getElapsedTime());
+    }
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+}
